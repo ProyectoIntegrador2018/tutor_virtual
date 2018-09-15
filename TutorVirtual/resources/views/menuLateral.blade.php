89ebe0c68c47ddc1603bff44465744110d8c0d8f
eGuajardoMozo <estefania_gm13@hotmail.com>
2018-09-15 17:49:24 -0500

    Menu Lateral pt1
    Primer parte del menu lateral, solo se creó la estructura y CSS, los elementos no hacen nada al hacer click. Se le dio la ruta de /menuLateral

---
+++ TutorVirtual/resources/views/menuLateral.blade.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" href="{{ asset('css/stylesheet.css') }}">
+
+<nav id="principal">
+    <ul>
+        <li>
+            <a href="">
+                <h3>Est. Acad.</h3>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <h3>Cursos</h3>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <h3>Grupos</h3>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <h3>Tutores</h3>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <h3>Alumnos</h3>
+            </a>
+        </li>
+    </ul>
+</nav>
--- TutorVirtual/routes/web.php
+++ TutorVirtual/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('menuLateral', function () {
+    return view('menuLateral');
+});
