3910b84697f1dd4cb4328d2fea079cff7152c68b
eGuajardoMozo <estefania_gm13@hotmail.com>
2018-09-19 20:54:24 -0500

    MenuLateralpt2
    Segunda parte del menu agregada. No hay nada funcional.

--- TutorVirtual/resources/views/menuLateral.blade.php
+++ TutorVirtual/resources/views/menuLateral.blade.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="{{ asset('css/stylesheet.css') }}">
 
 <nav id="principal">
-    <ul>
+    <ul class="menuHover">
         <li>
             <a href="">
                 <h3>Est. Acad.</h3>
@@ -29,3 +29,45 @@
         </li>
     </ul>
 </nav>
+
+<div class="secondMenu">
+    <ul id="uploadExcel">
+        <li>
+            <h2>Subir Excel</h2>
+        </li>
+        <li>
+            <input type="file"/>
+        </li>
+        <li>
+            <button type="submit">Submit</button>
+        </li>
+    </ul>
+    <ul>
+       <li class="menuHover2">
+            <h2>Sub-sección</h2>
+        </li>
+        <li class="menuHover2">
+            <h2>Sub-sección</h2>
+        </li>
+    </ul>
+    <ul>
+        <li>
+            <h2>Filtro 1</h2>
+        </li>
+        <li class="searchInput">
+            <input type="search"/>
+        </li>
+        <li>
+            <h2>Filtro 2</h2>
+        </li>
+        <li class="searchInput">
+            <input type="search"/>
+        </li>
+        <li>
+            <h2>Filtro 3</h2>
+        </li>
+        <li class="searchInput">
+            <input type="search"/>
+        </li>
+    </ul>
+</div>
