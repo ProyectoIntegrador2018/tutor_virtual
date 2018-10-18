1b57741f9f4db08b1543aa4d5555a065d4c7c4ea
EliasMera <a01280762@itesm.mx>
2018-10-17 20:30:25 -0500

    Fixes bug

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ko86t9azcob3a2f9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'a0dvdspxw82859qa'),
+            'username' => env('DB_USERNAME', 'efr0ofid3ud58dqe'),
+            'password' => env('DB_PASSWORD', 'pbu25snjb5uqtohm'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
