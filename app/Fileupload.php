770e5124881f5eefa2c12e5e370109bac1531ad9
Jorge Limón Cabrera <j.limon.cabrera@gmail.com>
2018-10-24 15:34:59 -0500

    Bugfix

--- app/Fileupload.php
+++ app/Fileupload.php
@@ -6,9 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fileupload extends Model
 {
-    Schema::create('fileuploads', function (Blueprint $table) {
-        $table->increments('id');
-        $table->string('filename');
-        $table->timestamps();
-    });
+    public function up() {
+        Schema::create('fileuploads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('filename');
+            $table->timestamps();
+        });
+    }
 }
