4798f7a5cc0e1c392a1fd2a86ab04fcb5c2ce983
EliasMera <a01280762@itesm.mx>
2018-11-04 13:50:39 -0600

    Fixes maincomp

--- resources/js/components/MainComponent.js
+++ resources/js/components/MainComponent.js
@@ -34,31 +34,17 @@ export default class MainComponent extends Component {
                         <Route path='/cursos' component={Curso_Sub}/>
                         <Route path='/alumnos' component={Alumno_Sub}/>
                         <Route path='/tutores' component={Tutores_Sub}/>
-                    {/* Main Navbar */}
-                    <div className="col-2 pt-4 px-1 bg-primary-dark h-100" name="mainNav">
-                        <ul className="list-group list-group-flush bg-transparent">
-                            <li className="list-group-item bg-transparent text-center">
-                                <Link to="/cursos" className="text-white h3">Cursos</Link>
-                            </li>
-                            <li className="list-group-item bg-transparent text-center">
-                                <Link to="/alumnos" className="text-white h3">Alumnos</Link>
-                            </li>
-                        </ul>
-                    </div>
                     
-                    {/* Sub Navbars - <div className="col-2 pt-4 px-1">*/}
-                    <Route path='/cursos' component={Curso_Sub}/>
-                    <Route path='/alumnos' component={Alumno_Sub}/>
 
                     {/* Content Areas - <div className="col-8 p-0">*/}
                     <Route exact path='/cursos' component={Cursos_Listing}/>
                     <Route exact path='/cursos/nuevo' component={Cursos_New}/>
 
-                        <Route exact path='/alumnos' component={Alumnos}/>
-                        <Route exact path='/alumnos/nuevo' component={Alumnos_New}/>
+                    <Route exact path='/alumnos' component={Alumnos}/>
+                    <Route exact path='/alumnos/nuevo' component={Alumnos_New}/>
 
-                        <Route exact path='/tutores' component={Tutores}/>
-                        <Route exact path='/tutores/nuevo' component={Tutores_New}/>
+                    <Route exact path='/tutores' component={Tutores}/>
+                    <Route exact path='/tutores/nuevo' component={Tutores_New}/>
                 </div>
             </Router>
         );
