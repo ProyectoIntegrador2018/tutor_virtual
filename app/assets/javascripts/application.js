2df753124cd186df4a1237c3e237a0d95ab33e53
hector <hectorortiz_96@hotmail.com>
2019-03-02 12:52:40 -0600

    Agregar Home y Dashboard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,14 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+// https://dribbble.com/shots/1640899-Freebie-Course-Dashboard
+
+$('nav a, .nav2 a').on('click', function() {
+  $(this).addClass('active');  $(this).parents('li').siblings().children('a').removeClass('active');
+});
+
+$(document).ready(function () {
+     if ($("[rel=tooltip]").length) {
+     $("[rel=tooltip]").tooltip();
+     }
+   });
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/dashboard'
   get 'static_pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  root 'static_pages#dashboard'
+  get "/dashboard", to: "static_pages#dashboard"
 end
