3de5a5e2c452b2c787f8a25306df8971693a46b8
hectorortiz96 <hectorortiz_96@hotmail.com>
2019-03-04 19:32:28 -0600

    Agregado Devise, verificacion de usuarios y sesiones

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/dashboard'
-  get 'static_pages/home'
+  devise_for :admins
+  devise_for :users
+  get 'static_pages/dashboard', :as => 'dashboard'
+  get 'static_pages/home',  :as => 'home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#dashboard'
-  get "/dashboard", to: "static_pages#dashboard"
 end
