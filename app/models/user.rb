1e22b3277ad46e3f07eb28be9736e5b47e505fff
Diego Contreras <czdiego.96@gmail.com>
2019-04-29 14:28:30 -0500

    empty master

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  enum role: [:user, :admin]
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
--- test/models/preinscrito_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PreinscritoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
