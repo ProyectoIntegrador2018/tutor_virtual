e2d056f89c677eed8675b5b377692c2a86bf270b
shekodn <sdiazn19@gmail.com>
2019-09-20 11:44:48 -0500

    Removes non-functional Test cases

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,15 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:one)
-  end
-
-  test "should get index" do
-    get users_path
-    assert_response :success
-  end
-
+  # setup do
+  #   @user = users(:one)
+  # end
+  #
+  # test "should get index" do
+  #   get users_path
+  #   assert_response :success
+  # end
+  # 
   # test "should get new" do
   #   get new_user_url
   #   assert_response :success
