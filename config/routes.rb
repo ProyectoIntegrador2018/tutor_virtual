228c0f0ca8ade7e7e4258f4ba7392c704ee91e7a
patoforbes <papato4bes@hotmail.com>
2019-09-27 19:48:11 -0500

    Preinscrito Borrar Todo
    Se Agrega la funcionabilidad Borar Todo en la pantalla de preinscritos

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 Rails.application.routes.draw do
 
-	
+
   resources :cursos
 	resources :sessions, only: [:new, :create, :destroy]
-	
+
 	resources :users do
 		collection { post :import }
 		collection { delete :destroy_multiple }
 	end
-	
+
 	resources :aliados do
 		collection { post :import }
 		collection { delete :destroy_multiple }
 	end
-	
+
 	resources :preinscritos do
 		collection { post :import }
+		collection { delete :destroy_all }
 		collection { delete :destroy_multiple }
+
 	end
 
 	resources :cursos do
@@ -28,4 +30,3 @@ Rails.application.routes.draw do
   	get 'logout', to: 'sessions#destroy', as: 'logout'
   	root 'static_pages#dashboard'
 end
-
