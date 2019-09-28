80ca03bed1196aa9829ebd382b85f485a9954e4a
Arturo González <arturogzztr@gmail.com>
2019-09-28 17:22:26 -0500

    Add lastnames to search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def index
     if params[:search]
-      @users = User.where('nombre LIKE ?', "%#{params[:search]}%")
+      @users = User.where('nombre LIKE ? OR apellido_paterno LIKE ? OR apellido_materno LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
     else
       @users = User.all
     end
