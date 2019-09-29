9ce073b47575a204b1bd66bd11abfae6cf589007
patoforbes <papato4bes@hotmail.com>
2019-09-29 11:32:51 -0500

    Correccion De CSV
    Permite importar CSV exportados por la aplicacion. Antes no se podia ya que al exporta se genera una linea nula que no ignoraba antes.

--- app/models/aliado.rb
+++ app/models/aliado.rb
@@ -1,6 +1,6 @@
 class Aliado < ApplicationRecord
 	def self.import(file)
-		CSV.foreach(file.path, headers: true, quote_char: '"') do |row|
+		CSV.foreach(file.path, headers: true, quote_char: '"', skip_blanks: true) do |row|
 			Aliado.create! row.to_hash
 		end
 	end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	def self.import(file)
-		CSV.foreach(file.path, headers: true, quote_char: '"') do |row|
+		CSV.foreach(file.path, headers: true, quote_char: '"', skip_blanks: true) do |row|
 			User.create! row.to_hash
 		end
 	end
