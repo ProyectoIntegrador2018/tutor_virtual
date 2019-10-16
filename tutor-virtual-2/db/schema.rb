73a843498231e8ca4a5a5a33f832a385a4594c99
Arturo González <arturogzztr@gmail.com>
2019-10-16 14:24:17 -0500

    Add student courses table

--- tutor-virtual-2/db/schema.rb
+++ tutor-virtual-2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_16_191700) do
+ActiveRecord::Schema.define(version: 2019_10_16_191914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2019_10_16_191700) do
     t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "courses_students", id: false, force: :cascade do |t|
+    t.bigint "course_id", null: false
+    t.bigint "student_id", null: false
+    t.index ["course_id", "student_id"], name: "index_courses_students_on_course_id_and_student_id"
+    t.index ["student_id", "course_id"], name: "index_courses_students_on_student_id_and_course_id"
   end
 
   create_table "stakeholders", force: :cascade do |t|
