d0f7b764ece1cb82d6ada9dc7c5b6cbc36be9469
Sergio Díaz <sdiazn19@gmail.com>
2019-10-16 19:37:40 -0500

    Add Authentication (#157)

--- tutor-virtual-2/config/routes.rb
+++ tutor-virtual-2/config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
-  resources :tutors
-  resources :supervisors
-  resources :stakeholders
-  resources :coordinators
-  resources :students
-  resources :courses
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/index'
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+      resources :tutors
+      resources :supervisors
+      resources :stakeholders
+      resources :coordinators
+      resources :students
+      resources :courses
+      get "/logout" => "devise/sessions#destroy"
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+      get "/login" => "devise/sessions#new"
+    end
+  end
 end
