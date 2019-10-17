90e4f9edcf72e22934044617e888293b315da467
shekodn <sdiazn19@gmail.com>
2019-10-16 19:32:29 -0500

    Add Authentication

--- tutor-virtual-2/app/controllers/application_controller.rb
+++ tutor-virtual-2/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+  private
+
+    def require_login
+      unless current_user || request.env['PATH_INFO'] == "/login"
+        redirect_to login_url
+      end
+    end
 end
