4b2bc2c522cc0c9a932fcbfe3adde3fcbb5130af
Arturo González <arturogzztr@gmail.com>
2019-10-20 18:10:49 -0500

    Add Coordinator tests

--- tutor-virtual-2/app/models/coordinator.rb
+++ tutor-virtual-2/app/models/coordinator.rb
@@ -1,2 +1,6 @@
 class Coordinator < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
--- tutor-virtual-2/test/models/coordinator_test.rb
+++ tutor-virtual-2/test/models/coordinator_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CoordinatorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @coordinator = Coordinator.new(email: "coordinator@example.com")
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @coordinator.email = valid_address
+      assert @coordinator.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
 end
