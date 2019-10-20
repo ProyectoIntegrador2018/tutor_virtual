02475ec0073ca986aa0cd42a178793b515351661
Arturo González <arturogzztr@gmail.com>
2019-10-20 18:51:56 -0500

    Fix coordinator test

--- tutor-virtual-2/config/routes.rb
+++ tutor-virtual-2/config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
   resources :coordinators
   resources :students
   resources :courses
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
