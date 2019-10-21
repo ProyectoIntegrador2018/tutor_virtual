93d63ad93b542f1e56a3c2164d758c2af5930b0d
Arturo González <arturogzztr@gmail.com>
2019-10-20 19:31:12 -0500

    Add supervisor tests

--- tutor-virtual-2/test/controllers/supervisors_controller_test.rb
+++ tutor-virtual-2/test/controllers/supervisors_controller_test.rb
@@ -15,14 +15,6 @@ class SupervisorsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create supervisor" do
-    assert_difference('Supervisor.count') do
-      post supervisors_url, params: { supervisor: { city: @supervisor.city, country: @supervisor.country, dob: @supervisor.dob, email: @supervisor.email, first_last_name: @supervisor.first_last_name, gender: @supervisor.gender, internal_password: @supervisor.internal_password, language: @supervisor.language, name: @supervisor.name, organization_code: @supervisor.organization_code, partner: @supervisor.partner, phone_number: @supervisor.phone_number, second_last_name: @supervisor.second_last_name, state: @supervisor.state, username: @supervisor.username } }
-    end
-
-    assert_redirected_to supervisor_url(Supervisor.last)
-  end
-
   test "should show supervisor" do
     get supervisor_url(@supervisor)
     assert_response :success
